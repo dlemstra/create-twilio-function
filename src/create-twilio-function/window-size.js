2ad5c4620624385ef71eab882eb7e9210b80a74d
Dirk Lemstra <dirk@lemstra.org>
2019-10-09 15:24:11 +0200

    Added missing space.

--- src/create-twilio-function/window-size.js
+++ src/create-twilio-function/window-size.js
@@ -8,8 +8,8 @@ function getWindowSize() {
     return defaultSize;
   }
   else {
-    if(!currentSize.width) currentSize.width = defaultSize.width;
-    if(!currentSize.height) currentSize.height = defaultSize.height;
+    if (!currentSize.width) currentSize.width = defaultSize.width;
+    if (!currentSize.height) currentSize.height = defaultSize.height;
     return currentSize;
   }
 }
